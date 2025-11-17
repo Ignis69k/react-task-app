c60f78186e57e9e2785c7e00cd158e4985a636b9
Patompong <224364417+Ignis69k@users.noreply.github.com>
2025-11-18 00:38:30 +0700

    Add useNavigate hook to Home component

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import Swal from 'sweetalert2'
 
 function Home() {
+  const navigate = useNavigate()  
   const [secretcode, setSecretcode] = useState('')
 
   const swalalerts = (alrt) => {
